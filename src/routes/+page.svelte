8c95c6ffabb0e61e5a38a7bd24ce180c37931baf
Darkness-2 <justinmmm690@gmail.com>
2023-03-29 19:58:32 -0400

    Add TailwindCSS and prettier plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-2xl font-bold text-blue-700">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,css,js,ts,svelte}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+};
