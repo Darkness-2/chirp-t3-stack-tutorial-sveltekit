a721d38e635570238d6cecc43457b210a5271f02
Darkness-2 <justinmmm690@gmail.com>
2023-03-29 21:54:08 -0400

    Add Tanstack query

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import { trpc } from '$lib/trpc/trpc.client';
 	import type { PageData } from './$types';
+	import { createQuery } from '@tanstack/svelte-query';
 
 	export let data: PageData;
+
+	const query = createQuery({
+		queryKey: ['hello'],
+		queryFn: () => trpc.hello.query()
+	});
 </script>
 
 <div class="flex flex-col gap-4">
@@ -12,10 +18,21 @@
 	<div>
 		<p>Example of CSR using Svelte #await</p>
 		{#await trpc.hello.query()}
-			<div>Loading...</div>
+			<p>Loading...</p>
 		{:then data}
 			<p>{`${data} from CSR`}</p>
 		{/await}
+	</div>
+
+	<div>
+		<p>Example of CSR using Tanstack Query</p>
+		{#if $query.isLoading}
+			<p>Loading...</p>
+		{:else if $query.isError}
+			<p>{$query.error}</p>
+		{:else if $query.isSuccess}
+			<p>{`${$query.data} from Tanstack`}</p>
+		{/if}
 	</div>
 
 	<div>
