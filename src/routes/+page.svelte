4e6ac3369ce92271fc0900c52728a9b4a5387cb9
Darkness-2 <justinmmm690@gmail.com>
2023-03-29 21:37:22 -0400

    Add example data loading from +page.server.ts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
 	import { trpc } from '$lib/trpc/trpc.client';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
-<h1 class="text-2xl font-bold text-blue-700">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col gap-4">
+	<h1 class="text-2xl font-bold text-blue-700">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-{#await trpc.hello.query()}
-	<div>Loading...</div>
-{:then data}
-	{data}
-{/await}
+	<div>
+		<p>Example of CSR using Svelte #await</p>
+		{#await trpc.hello.query()}
+			<div>Loading...</div>
+		{:then data}
+			<p>{`${data} from CSR`}</p>
+		{/await}
+	</div>
+
+	<div>
+		<p>Example of SSR using tRPC and +page.server.ts</p>
+		<p>{data.pageServerLoadExample}</p>
+	</div>
+</div>
