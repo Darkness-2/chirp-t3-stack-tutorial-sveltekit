35a986852185885c12ff047d6fb1be2a1b003d2e
Darkness-2 <justinmmm690@gmail.com>
2023-03-31 20:05:24 -0400

    Catch 404s for post page

--- src/routes/post/[id]/+page.server.ts
+++ src/routes/post/[id]/+page.server.ts
@@ -1,18 +1,33 @@
 import { postsRouter } from '$lib/trpc/routers/posts';
 import { createTRPCContext } from '$lib/trpc/trpc.server';
+import { error } from '@sveltejs/kit';
+import { TRPCError } from '@trpc/server';
 import type { PageServerLoad } from './$types';
 
 const FIVE_MINUTES_IN_SECONDS = 5 * 60;
 
 export const load: PageServerLoad = async (event) => {
 	const caller = postsRouter.createCaller(createTRPCContext());
-	const post = await caller.getById({ id: event.params.id });
 
-	event.setHeaders({
-		'cache-control': `max-age=${FIVE_MINUTES_IN_SECONDS}, must-revalidate`
-	});
+	try {
+		const post = await caller.getById({ id: event.params.id });
 
-	return {
-		post
-	};
+		event.setHeaders({
+			'cache-control': `max-age=${FIVE_MINUTES_IN_SECONDS}, must-revalidate`
+		});
+
+		return {
+			post
+		};
+	} catch (e) {
+		if (e instanceof TRPCError && e.code === 'NOT_FOUND') {
+			throw error(404, {
+				message: 'Post not found'
+			});
+		} else {
+			throw error(500, {
+				message: 'Something went wrong'
+			});
+		}
+	}
 };
