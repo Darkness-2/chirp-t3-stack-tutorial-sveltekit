a35aecff1fb8cd8d6d1ddcc0fd5318aebf46e19d
Darkness-2 <justinmmm690@gmail.com>
2023-04-04 17:41:46 -0400

    Add ts-reset

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,8 @@
-// See https://kit.svelte.dev/docs/types#app
-
+import '@total-typescript/ts-reset';
 import type { AppRouter } from '$lib/server/trpc/routers/_app';
 import type { User } from '@supabase/supabase-js';
 
+// See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
